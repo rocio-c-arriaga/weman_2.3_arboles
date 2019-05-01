921306a8bf32b1efd76e2b88dae7ec6cc20dd7f5
rocio-c-arriaga <rociocastroarriaga@gmail.com>
2019-05-01 16:17:56 -0500

    avance

---
+++ js/arboles.js
@@ -0,0 +1,50 @@
+//obtiene el momento actual
+var ahora = new Date();
+//Obtiene el momento de después de tiempo
+
+class Tree{
+	constructor(){
+		//Genera el arbol como tal
+		this.root = null;
+
+	}
+
+	ad (node) {
+		//funciones para agregar nodos...
+
+		}
+
+// aqui se ejecuta el contructor de Tree
+var Tree = new Tree();
+Tree.add("prueba");
+
+
+function Tree() {
+	this.root = null;
+}
+
+add(val){
+	if (this.isEmpty()){
+		this.root=new Node(val)
+		return
+	}
+}
+var personas = this.root
+while (aux) {
+	// a la izquierda
+	if (val < aux.value){
+		if (aux=aux.left)
+	} else {
+		aux.left=new Node (val)
+		return
+	}
+} else {
+	// vamos hacia la derecha
+	if (aux.right) {
+		aux = aux.right
+	} else {
+		aux.right=new Node (val)
+		return
+	}
+   
+  
--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 //Esta variable guarda una referencia al control de resultado, se declara global para no tener que estar ejecutando document.getElementById en cada ocasión que se necesite.
+import BinarySearchTreeNode from './BinarySearchTreeNode';
+
+export default class BinarySearchTreeNode {
+	/**
+	*@param {function} [nodeValueCompareFunction]
+	*/
+	constructor(nodeValueCompareFunction){
+		this.root = new BinarySearchTreeNode(null, nodeValueCompareFunction);
+
+		//Steal node comparator from the root.
+		this.nodeComparator = this.root.nodeComparator;
+	}
+
+	/**
+	*@param {*} value
+	*@return {BinarySearchTreeNode}
+	*/
+	insert(value) {
+		return this.root.insert(value);
+
+	}
+
+
+	/**
+	* @param {*} value
+	* @return this.root insert (value);
+	*/
+
+	contains (value){
+		return this.root.contains (value);
+	}
+
+	/** 
+	* @parum {*} value
+	* @return {boolean}
+	*/
+	removed(value) {
+		return this.root.remove(value);
+
+	}
+
+	/**
+	* @return {string}
+	*/
+	toString() {
+		return this.root.toString();
+	}
+	
+}
+
